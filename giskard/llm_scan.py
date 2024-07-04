54e58e39fc9509bae1e071ed4f46c7b4097bb670
Mikko Lempinen <milempin@student.oulu.fi>
2024-07-04 11:39:16 +0300

    giskard part analysis

--- giskard/llm_scan.py
+++ giskard/llm_scan.py
@@ -11,6 +11,7 @@ import pandas as pd
 import giskard
 from openai import OpenAI
 from giskard.llm.client.openai import OpenAIClient
+from giskard.scanner import Scanner
 
 MODEL = "phi3" # Tag of the LLM to scan, see https://ollama.com/library for available models.
 MODEL_NAME = "Standard Phi-3 Model" # Used by the scan to generate domain-specific tests.
@@ -98,6 +99,12 @@ giskard_model = giskard.Model(
 )
 
 if __name__=="__main__":
-    # Perform Giskard scan
-    scan_results = giskard.scan(giskard_model, giskard_dataset)
+    # Perform full Giskard scan
+    #scan_results = giskard.scan(giskard_model, giskard_dataset)
+    #scan_results.to_html("giskard_scan_results.html")
+
+
+    # Perform specific vulnerability scan(s)
+    scanner = Scanner(None, only=['performance'])
+    scan_results = scanner.analyze(giskard_model, dataset=giskard_dataset)
     scan_results.to_html("giskard_scan_results.html")
